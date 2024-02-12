39bf9c29d4c943e981c9e9d8a13afc75db396aa1
alore <alore@DESKTOP-FOAKBLF>
2024-02-12 14:21:00 +0100

    Desafío 1 Ud 2

---
+++ PrimerosPasos/src/Desafios/Desafio1Ud2.java
@@ -0,0 +1,15 @@
+package Desafios;
+public class Desafio1Ud2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=10;
+		
+		while(i>=1 && i<=10) {
+			System.out.println(i);
+			i--;
+		}
+		
+	}
+
+}
